08ee8a798f321a02ecdc7a4eea41ede056db9646
Khushboomahajan19 <soniamahajan1281@gmail.com>
2024-07-18 16:02:02 +0530

    auth updated

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,7 +6,7 @@ const auth = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer "))
     throw new BadRequestError("No token provided");
-  const token = authHeader.split("")[1];
+  const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const { id, username } = decoded;
